44125be234eb3e3a9da2e177d9575621aded5d9d
Jaikanth S <jaikanth.sekar@tecnotree.com>
2020-02-08 13:23:15 +0530

    Add project files.

---
+++ MyFirstVisualizer/DebuggerSide.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using System.Windows.Forms;
+
+[assembly: System.Diagnostics.DebuggerVisualizer(
+typeof(MyFirstVisualizer.DebuggerSide),
+typeof(VisualizerObjectSource),
+Target = typeof(System.String),
+Description = "My First Visualizer")]
+
+namespace MyFirstVisualizer
+{
+    public class DebuggerSide : DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            MessageBox.Show(objectProvider.GetObject().ToString(), "Goat");
+        }
+
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            VisualizerDevelopmentHost visualizerHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(DebuggerSide));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
---
+++ MyTestConsole/TestConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFirstVisualizer;
+
+namespace MyTestConsole
+{
+    class TestConsole
+    {
+        static void Main(string[] args)
+        {
+            String myString = "Hello, World";
+            DebuggerSide.TestShowVisualizer(myString);
+        }
+    }
+}
